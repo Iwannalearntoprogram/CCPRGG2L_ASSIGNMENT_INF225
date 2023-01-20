11dd29ab0d5cb1a64cc26c6ea081f645954d09b7
Carl Jay Margate <76047427+Iwannalearntoprogram@users.noreply.github.com>
2023-01-20 11:28:22 +0800

    Create Character.java

---
+++ Exercise3/Character.java
@@ -0,0 +1,19 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    
+    public void sayMyStrength() {
+        System.out.println("My strength is " + strength);
+    }
+    void Atk(){
+        System.out.println("I am Attacking");
+
+    }
+
+}
